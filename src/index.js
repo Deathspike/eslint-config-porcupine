0dd55b3b619ebcf67abaf240fc74994c871efd4e
Deathspike <meister.deathspike@outlook.com>
2025-10-10 11:29:40 +0200

    fix: explicitly export porcupine type

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { Linter } from "eslint";
 import { eslintConfig } from "./configs/eslintConfig.js";
 import { jsdocConfig } from "./configs/jsdocConfig.js";
 import { tscConfig } from "./configs/tscConfig.js";
 
+/** @returns {Linter.Config[]} */
 export function porcupine() {
   return [...eslintConfig(), ...jsdocConfig(), ...tscConfig()];
 }
