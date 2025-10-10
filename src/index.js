2e27bb29aed3cc5cbe8b34a5bbd5da3d3c070985
Deathspike <meister.deathspike@outlook.com>
2025-10-10 11:42:47 +0200

    feat: add prettier config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { Linter } from "eslint";
 import { eslintConfig } from "./configs/eslintConfig.js";
 import { jsdocConfig } from "./configs/jsdocConfig.js";
+import prettier from "eslint-config-prettier";
 import { tscConfig } from "./configs/tscConfig.js";
 
 /** @returns {Linter.Config[]} */
 export function porcupine() {
-  return [...eslintConfig(), ...jsdocConfig(), ...tscConfig()];
+  return [...eslintConfig(), ...jsdocConfig(), ...tscConfig(), prettier];
 }
