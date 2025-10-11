19823a0eda0a2707af52c29e0b6625a2c522741f
Deathspike <meister.deathspike@outlook.com>
2025-10-11 12:03:14 +0200

    refactor: remove redunant globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from "eslint/config";
-import globals from "globals";
 
 import { porcupine } from "./src/index.js";
 
-export default defineConfig([
-  ...porcupine(),
-  { languageOptions: { globals: { ...globals.node } } },
-]);
+export default defineConfig(porcupine());
